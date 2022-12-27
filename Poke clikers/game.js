bbbba5c7c16825fb3428bb900d41dc2405e317f5
rayane-dev <95442692+rayane-dev@users.noreply.github.com>
2022-12-27 16:27:42 +0100

    Add files via upload

---
+++ Poke clikers/game.js
@@ -0,0 +1,27 @@
+// let containerBaie = document.querySelector('#container-baie');
+// setInterval(baieTimer, 1000);
+// let point = 0;
+// parseInt(point);
+// let score = document.querySelector('#score');
+
+
+// // if(localStorage.getItem('point')){
+// //     // Si un score est enregistré afficher le score
+// //     score.innerHTML = point;
+// //  }
+
+// containerBaie.addEventListener('click', () => {
+//     point++
+//     score.innerHTML = point;
+//  });
+
+// function baieTimer(){
+//     point++
+//     score.innerHTML = point;
+//  }
+
+
+
+ 
+// console.log(localStorage.getItem('Username'));
+ 
